d592c7ab0f9846799496da40a613f311864c4d62
YS <kakaru0190@gmail.com>
2022-10-28 15:03:24 +0900

    [level 0] Title: 암호 해독, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -0,0 +1,9 @@
+function solution(cipher, code) {
+    var answer = '';
+    
+    for(let i = code - 1; i < cipher.length; i += code) {
+        answer += cipher[i]
+    }
+    
+    return answer;
+}
