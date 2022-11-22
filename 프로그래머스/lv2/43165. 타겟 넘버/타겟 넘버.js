4639612197e1ac01d1d243dd6473536a741fc172
YS <kakaru0190@gmail.com>
2022-11-23 00:08:19 +0900

    [level 2] Title: 타겟 넘버, Time: 56.05 ms, Memory: 65.7 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,15 +1,38 @@
 function solution(numbers, target) {
-  let answer = 0;
-  getAnswer(0,0);
-  function getAnswer(x,value) {
-      if(x<numbers.length){
-          getAnswer(x+1,value + numbers[x]);
-          getAnswer(x+1,value - numbers[x]);
-      } else{
-          if(value === target){
-              answer++
-          }
-      }
+  const arr1 = [];
+  const arr2 = [];
+
+  for (let i = 0; i <= numbers.length; i++) {
+    const val = combination(numbers, i);
+    !val.length
+      ? arr1.push(0)
+      : val.forEach((item) => {
+          arr1.push(item.reduce((acc, cur) => acc + cur, 0));
+        });
+
+    const val2 = combination(numbers, numbers.length - i).reverse();
+    !val2.length
+      ? arr2.push(0)
+      : val2.forEach((item) => {
+          arr2.push(item.reduce((acc, cur) => acc - cur, 0));
+        });
   }
-  return answer;
+
+  return arr1.map((item, i) => item + arr2[i]).filter((item) => item === target)
+    .length;
 }
+
+const combination = (arr, num) => {
+  const result = [];
+  if (num === 1) return arr.map((item) => [item]);
+
+  arr.forEach((val, idx, origin) => {
+    const rest = origin.slice(idx + 1);
+    const recurResult = combination(rest, num - 1);
+    const attach = recurResult.map((recurVal) => [val, ...recurVal]);
+
+    result.push(...attach);
+  });
+
+  return result;
+};
