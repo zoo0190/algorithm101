c15cc0eb912cdce87d6c8cbe01127254ac731b23
YS <kakaru0190@gmail.com>
2023-04-22 19:31:20 +0900

    [unrated] Title: 문자열 반복해서 출력하기, Time: 50.06 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    str = input[0];
+    n = Number(input[1]);
+    let a = ''
+    for (let i = 1; i <= n; i++) {
+        a += str
+    }
+    console.log(a)
+});
