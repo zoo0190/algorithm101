7090d20d53085bb5146c662552291d5a5070e975
YS <kakaru0190@gmail.com>
2023-04-23 13:28:47 +0900

    [unrated] Title: 문자열 돌리기, Time: 42.41 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    for(let st of str) {
+        console.log(st)
+    }
+});
